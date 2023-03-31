f7614f40c1664673d5265bc170ad10ef02452cfe
James Reed <jamesreed.3050@gmail.com>
2023-03-31 21:03:00 +0900

    initial login form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
@@ -13,15 +14,40 @@ const App = () => {
     )
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log('Logging in...', username, password)
+  }
+
 
   return (
     <div>
       <h2>blogs</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username:
+            <input
+            type='text'
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+            />
+        </div>
+        <div>
+          password:
+            <input
+            type='text'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
-}
+  );
+};
 
 export default App
